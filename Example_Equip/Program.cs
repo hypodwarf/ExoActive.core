d747332b166f03e276c2d186c8f74d11769b6c74
hypodwarf <hypodwarf@gmail.com>
2021-06-28 19:42:23 -0600

    Improved messaging for equip example.

--- Example_Equip/Program.cs
+++ Example_Equip/Program.cs
@@ -7,6 +7,8 @@ namespace Example_Equip
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("An actor is capable of equipping 2 ring and 1 belt.");
+            
             Actor actor = new Actor();
             Ring ring1 = new Ring();
             Ring ring2 = new Ring();
@@ -14,17 +16,20 @@ namespace Example_Equip
             Belt belt1 = new Belt();
             Belt belt2 = new Belt();
 
-            Console.WriteLine(Capability.PerformAction<Equip.EquipItem>(actor, ring1));
-            Console.WriteLine(Capability.PerformAction<Equip.EquipItem>(actor, ring2));
-            Console.WriteLine(Capability.PerformAction<Equip.EquipItem>(actor, ring3));
-            Console.WriteLine(Capability.PerformAction<Equip.EquipItem>(actor, belt1));
-            Console.WriteLine(Capability.PerformAction<Equip.EquipItem>(actor, belt2));
+            Console.WriteLine($"Equipped 1st ring: {Capability.PerformAction<Equip.EquipItem>(actor, ring1)}");
+            Console.WriteLine($"Equipped 2nd ring: {Capability.PerformAction<Equip.EquipItem>(actor, ring2)}");
+            Console.WriteLine($"Equipped 3rd ring: {Capability.PerformAction<Equip.EquipItem>(actor, ring3)}");
+            Console.WriteLine($"Equipped 1st belt: {Capability.PerformAction<Equip.EquipItem>(actor, belt1)}");
+            Console.WriteLine($"Equipped 2nd belt: {Capability.PerformAction<Equip.EquipItem>(actor, belt2)}");
             
-            Console.WriteLine(Capability.PerformAction<Equip.UnequipItem>(actor, ring1));
-            Console.WriteLine(Capability.PerformAction<Equip.UnequipItem>(actor, ring2));
-            Console.WriteLine(Capability.PerformAction<Equip.UnequipItem>(actor, ring3));
-            Console.WriteLine(Capability.PerformAction<Equip.UnequipItem>(actor, belt1));
-            Console.WriteLine(Capability.PerformAction<Equip.UnequipItem>(actor, belt2));
+            Console.WriteLine($"Unequipped 1st ring: {Capability.PerformAction<Equip.UnequipItem>(actor, ring1)}");
+            Console.WriteLine($"Equipped 3rd ring: {Capability.PerformAction<Equip.EquipItem>(actor, ring3)}");
+            
+            Console.WriteLine($"Unequipped 1st ring: {Capability.PerformAction<Equip.UnequipItem>(actor, ring1)}");
+            Console.WriteLine($"Unequipped 2nd ring: {Capability.PerformAction<Equip.UnequipItem>(actor, ring2)}");
+            Console.WriteLine($"Unequipped 3rd ring: {Capability.PerformAction<Equip.UnequipItem>(actor, ring3)}");
+            Console.WriteLine($"Unequipped 1st belt: {Capability.PerformAction<Equip.UnequipItem>(actor, belt1)}");
+            Console.WriteLine($"Unequipped 2nd belt: {Capability.PerformAction<Equip.UnequipItem>(actor, belt2)}");
         }
     }
 }
